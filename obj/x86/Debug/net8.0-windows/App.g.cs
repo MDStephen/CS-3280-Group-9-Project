f8a1e8e0c22364ecdce6342cd44da1a15a0691f1
aerofight <mahlonstephenson@mail.weber.edu>
2026-03-25 14:22:08 -0600

    config files

--- obj/x86/Debug/net8.0-windows/App.g.cs
+++ obj/x86/Debug/net8.0-windows/App.g.cs
@@ -45,7 +45,7 @@ namespace CS_3280_Group_9_Project {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.8.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.13.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace CS_3280_Group_9_Project {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.8.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.13.0")]
         public static void Main() {
             CS_3280_Group_9_Project.App app = new CS_3280_Group_9_Project.App();
             app.InitializeComponent();
