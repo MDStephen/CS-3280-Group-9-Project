9c2751a5e9b9f21de3976e26a514953a33bcb259
XaRodriguezWSU <xanderrodriguez@mail.weber.edu>
2026-03-25 11:49:04 -0600

    Configure items window. Sets up logic class and SQL helper functions

--- Items/clsItemsLogic.cs
+++ Items/clsItemsLogic.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +10,160 @@ namespace CS_3280_Group_9_Project.Items
 {
     internal class clsItemsLogic
     {
+        /// <summary>
+        /// Method to get a list of all items in the database
+        /// </summary>
+        /// <returns>A list of items</returns>
+        /// <exception cref="Exception"></exception>
+        public static List<clsItem> GetAllItems()
+        {
+            try
+            {
+                // Create a database
+                clsDataAccess db = new clsDataAccess();
+
+                // Create a list to hold the passenger information
+                List<clsItem> items = new List<clsItem>();
+
+                // Create a DataSet to hold the results of the SQL query
+                DataSet ds;
+                int iReturnValue = 0;
+
+                // Get the SQL statement for retrieving flight information
+                string sSQL = clsItemsSQL.GetItems();
+
+                // Execute the SQL statement and populate the DataSet
+                ds = db.ExecuteSQLStatement(sSQL, ref iReturnValue);
+
+                // Loop through the DataSet and populate the list of passengers
+                foreach (DataRow row in ds.Tables[0].Rows)
+                {
+                    clsItem item = new clsItem
+                    {
+                        iItemCode = Convert.ToInt32(row["Item_Code"]),
+                        dItemCost = Convert.ToDecimal(row["Item_Cost"]),
+                        sItemDescription = row["Item_Description"].ToString()
+                    }
+                    ;
+                    items.Add(item);
+                }
+
+                return items;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." +
+                                    MethodInfo.GetCurrentMethod().Name + "-> " + ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Method to add an item to the database
+        /// </summary>
+        /// <param name="newItem">New item to add to the database</param>
+        /// <exception cref="Exception"></exception>
+        public static void AddItem(clsItem newItem)
+        {
+            try
+            {
+                // Create a database
+                clsDataAccess db = new clsDataAccess();
+
+                // Get the SQL statement for adding an item
+                string sSQL = clsItemsSQL.AddItem(newItem);
+
+                // Execute the SQL statement
+                db.ExecuteNonQuery(sSQL);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." +
+                                    MethodInfo.GetCurrentMethod().Name + "-> " + ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Method to edit an item in the database
+        /// </summary>
+        /// <param name="oldItem">Old item to update</param>
+        /// <param name="newItem">New item to update to</param>
+        /// <exception cref="Exception"></exception>
+        public static void EditItem(clsItem oldItem, clsItem newItem)
+        {
+            try
+            {
+                // Create a database
+                clsDataAccess db = new clsDataAccess();
+
+                // Get the SQL statement for editing an item
+                string sSQL = clsItemsSQL.UpdateItem(oldItem, newItem);
+
+                // Execute the SQL statement
+                db.ExecuteNonQuery(sSQL);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." +
+                                    MethodInfo.GetCurrentMethod().Name + "-> " + ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Method to delete an item from the database
+        /// </summary>
+        /// <param name="itemToDelete">Item to delet</param>
+        /// <exception cref="Exception"></exception>
+        public static void DeleteItem(clsItem itemToDelete)
+        {
+            try
+            {
+                // Create a database
+                clsDataAccess db = new clsDataAccess();
+
+                // Get the SQL statement for deleting an item
+                string sSQL = clsItemsSQL.DeleteItem(itemToDelete);
+
+                // Execute the SQL statement
+                db.ExecuteNonQuery(sSQL);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." +
+                                    MethodInfo.GetCurrentMethod().Name + "-> " + ex.Message);
+            }
+        }
+        
+        /// <summary>
+        /// Method to determine whether an item is on an existing invoice or not
+        /// </summary>
+        /// <param name="itemToCheck">Item to check for invoices</param>
+        /// <returns>True if the item exists on an invoice, false if otherwise</returns>
+        /// <exception cref="Exception"></exception>
+        public static bool IsItemOnInvoice(clsItem itemToCheck)
+        {
+            try
+            {
+                // Create a database
+
+                clsDataAccess db = new clsDataAccess();
+                // Get the SQL statement for checking if an item is on an invoice
+                string sSQL = clsItemsSQL.GetInvoicesWithItem(itemToCheck);
+
+                // Execute the SQL statement and get the results
+                DataSet ds;
+                int iReturnValue = 0;
+                ds = db.ExecuteSQLStatement(sSQL, ref iReturnValue);
+
+                // If there are any rows returned, then the item is on an invoice
+                return ds.Tables[0].Rows.Count > 0;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." +
+                                    MethodInfo.GetCurrentMethod().Name + "-> " + ex.Message);
+            }
+        }
+
+        // Create a method that handles refreshing the data grid once the item list has been updated (added, edited, or deleted)
     }
 }
--- Items/wndItems.xaml.cs
+++ Items/wndItems.xaml.cs
@@ -19,9 +19,82 @@ namespace CS_3280_Group_9_Project.Items
     /// </summary>
     public partial class wndItems : Window
     {
+        // Initializae ItemsLogic class to access its methods
+
         public wndItems()
         {
+            // Initialize the window and its components
+            // TODO: configure properly
             InitializeComponent();
+
+            // Populate the data grid with the list of items from the database
+        }
+
+        /// <summary>
+        /// Private boolean variable to track whether any changes to the list has occurred
+        /// </summary>
+        private bool bHasItemListChanged;
+
+        /// <summary>
+        /// Public boolean variable to send to main UI to update whether any changes have occurred so that visuals/data can be updated
+        /// </summary>
+        public bool HasItemListChanged;
+
+        /// <summary>
+        /// Function to allow user to add an item
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            // Populate descriptive title label
+
+            // Clear textboxes and make them so they can be written into
+
+            // Prompt user to click the save button to save the new item to the database
+
+            // Allow Save button to be clicked
+
+        }
+
+        /// <summary>
+        /// Function to allow user to edit the current details of a selected item
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            // Populate descriptive title label
+
+            // Identify current item
+
+            // Keep current data in text boxes and allow them to be written into/changed
+
+            // Allow Save button to be clicked
+        }
+
+        /// <summary>
+        /// Function to save the current data in the text boxes to the database
+        /// </summary>
+        /// <remarks>
+        /// This button should become available and should be clicked AFTER either the add or save button
+        /// </remarks>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            // The save button should be able to handle both adding and editing an item, so we will need to determine which action the user is trying to perform
+
+            // Determine what action the user is trying to perform (add or edit)
+
+            // Create a new item based on the data in the text boxes
+
+            // If the user is trying to add an item, add the new item to the database
+
+            // If the user is trying to edit an item, update the existing item in the database with the new data
+
+            // Set the boolean variable to true to indicate that a change has been made to the item list
+            
         }
     }
 }
