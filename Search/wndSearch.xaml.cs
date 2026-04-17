1089d60b567d00e79084834a106a5d371790fe5b
aerofight <mahlonstephenson@mail.weber.edu>
2026-04-17 16:16:26 -0600

    fix: missing try catch statements, missing clear filters logic and returning database index now instead of listbox index

--- Search/wndSearch.xaml.cs
+++ Search/wndSearch.xaml.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -27,7 +28,7 @@ namespace CS_3280_Group_9_Project.Search
         /// <summary>
         /// An integer storing the ID of the selected invoice which will be returned to main (parent window)
         /// </summary>
-        public int SelectedInvoiceID = 1;
+        public int SelectedInvoiceID = -1;
 
         /// <summary>
         /// A list for populating with data from the database
@@ -40,27 +41,37 @@ namespace CS_3280_Group_9_Project.Search
         public wndSearch()
         {
             InitializeComponent();
-            warningFlag.Content = "";
-            clsSearchLogic sL = new clsSearchLogic();
-            invoiceList = sL.GetInvoiceList();
-            invoiceListDisplay.ItemsSource = invoiceList;
 
-            // Initialize the search limiting combo boxes
-            invoiceList = invoiceList.OrderBy(x => x.TotalCost).ToList();
-
-            invoice_number_combo_box.Items.Add("All");
-            invoice_date_combo_box.Items.Add("All");
-            total_cost_combo_box.Items.Add("All");
-
-            invoice_number_combo_box.SelectedValue = "All";
-            invoice_date_combo_box.SelectedValue = "All";
-            total_cost_combo_box.SelectedValue = "All";
-
-            for (int i = 0; i < invoiceList.Count; i++) 
+            try
             {
-                invoice_number_combo_box.Items.Add(invoiceList[i].ID.ToString());
-                invoice_date_combo_box.Items.Add(invoiceList[i].Date);
-                total_cost_combo_box.Items.Add(invoiceList[i].TotalCost.ToString());
+                warningFlag.Content = "";
+                clsSearchLogic sL = new clsSearchLogic();
+                invoiceList = sL.GetInvoiceList();
+                invoiceListDisplay.ItemsSource = invoiceList;
+
+                // Initialize the search limiting combo boxes
+                invoiceList = invoiceList.OrderBy(x => x.TotalCost).ToList();
+
+                invoice_number_combo_box.Items.Add("All");
+                invoice_date_combo_box.Items.Add("All");
+                total_cost_combo_box.Items.Add("All");
+
+                invoice_number_combo_box.SelectedValue = "All";
+                invoice_date_combo_box.SelectedValue = "All";
+                total_cost_combo_box.SelectedValue = "All";
+
+                for (int i = 0; i < invoiceList.Count; i++)
+                {
+                    invoice_number_combo_box.Items.Add(invoiceList[i].ID.ToString());
+                    invoice_date_combo_box.Items.Add(invoiceList[i].Date);
+                    total_cost_combo_box.Items.Add(invoiceList[i].TotalCost.ToString());
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." +
+                                    MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+
             }
         }
 
@@ -92,45 +103,95 @@ namespace CS_3280_Group_9_Project.Search
         /// <param name="e"></param>
         private void limitSearch(object sender, SelectionChangedEventArgs e)
         {
-            if (invoice_number_combo_box.SelectedItem == null || invoice_date_combo_box.SelectedItem == null || total_cost_combo_box.SelectedItem == null)
+            try
             {
-                return;
-            }
+                if (invoice_number_combo_box.SelectedItem == null || invoice_date_combo_box.SelectedItem == null || total_cost_combo_box.SelectedItem == null)
+                {
+                    return;
+                }
 
-            int id;
-            int total_cost;
-            string date;
+                int id;
+                int total_cost;
+                string date;
 
-            if ((string)invoice_number_combo_box.SelectedItem == "All")
-            {
-                id = -1;
+                if ((string)invoice_number_combo_box.SelectedItem == "All")
+                {
+                    id = -1;
+                }
+                else
+                {
+                    id = int.Parse(invoice_number_combo_box.SelectedItem.ToString());
+                }
+                if ((string)invoice_date_combo_box.SelectedItem == "All")
+                {
+                    date = null;
+                }
+                else
+                {
+                    date = (string)invoice_date_combo_box.SelectedItem;
+                }
+                if ((string)total_cost_combo_box.SelectedItem == "All")
+                {
+                    total_cost = -1;
+                }
+                else
+                {
+                    total_cost = int.Parse(total_cost_combo_box.SelectedItem.ToString());
+                }
+
+                invoiceListDisplay.ItemsSource = invoiceList
+                    .Where(x => (id == -1 || x.ID == id)
+                             && (date == null || x.Date == date)
+                             && (total_cost == -1 || x.TotalCost == total_cost))
+                    .ToList();
             }
-            else
-            {
-                id = int.Parse(invoice_number_combo_box.SelectedItem.ToString());
+            catch (Exception ex)
+{
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." +
+                                    MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+
             }
-            if((string)invoice_date_combo_box.SelectedItem == "All")
+        }
+
+        /// <summary>
+        /// Sets the internal value to the value selected from the list box
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void invoiceListDisplaySelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            try
             {
-                date = null;
+                var item = ((int ID, string Date, int TotalCost))invoiceListDisplay.SelectedItem;
+                SelectedInvoiceID = item.ID;
             }
-            else
+            catch (Exception ex)
+{
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." +
+                                    MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+
+            }
+        }
+
+        /// <summary>
+        /// Sets filters to all essentially 'clearing' them
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void clearFiltersClicked(object sender, RoutedEventArgs e)
+        {
+            try
             {
-                date = (string)invoice_date_combo_box.SelectedItem;
+                invoice_date_combo_box.SelectedItem = "All";
+                invoice_number_combo_box.SelectedItem = "All";
+                total_cost_combo_box.SelectedItem = "All";
             }
-            if ((string)total_cost_combo_box.SelectedItem == "All")
-            {
-                total_cost = -1;
+            catch (Exception ex)
+{
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." +
+                                    MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+
             }
-            else
-            {
-                total_cost = int.Parse(total_cost_combo_box.SelectedItem.ToString());
-            }
-            
-            invoiceListDisplay.ItemsSource = invoiceList
-                .Where(x => (id == -1 || x.ID == id)
-                         && (date == null || x.Date == date)
-                         && (total_cost == -1 || x.TotalCost == total_cost))
-                .ToList();
         }
     }
 }
